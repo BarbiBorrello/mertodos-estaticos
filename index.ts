f28d8191e9993830c415e547a281032e682ec678
Barbi Borrello <biborrello@hotmail.com>
2024-05-21 18:04:34 -0300

    metodos estaticos

--- index.ts
+++ index.ts
@@ -9,14 +9,27 @@ class Peli {
   getTitle(): string {
     return this.title;
   }
+
+  //---metodo estatico : static  se puede usar sin instanciar el objeto Peli (GRUPOS) --//
+
+  static searchPelis():Peli[]{
+    const peliEncontrada = new Peli("Peli encontrada");
+    return [peliEncontrada];
+
+  }
 }
 
 function main() {
-  const miPeli = new Peli("Una Peli");
-  console.log(miPeli.getTitle());
-  console.log ("soy el cambio n° 2");
-  console.log ("soy el cambio n° 3");
-  console.log ("soy un cambio desde GitHub")
+  // const miPeli = new Peli("Una Peli");
+  // console.log(miPeli.getTitle());
+  // console.log ("soy el cambio n° 2");
+  // console.log ("soy el cambio n° 3");
+  // console.log ("soy un cambio desde GitHub")
+
+  const pelis = Peli.searchPelis();
+  // console.log(pelis);
+  console.log(pelis[0]);
+  
 }
 
 main();
